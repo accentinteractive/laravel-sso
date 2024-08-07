de73172f491744904d3e2c442ff981e081530967
Joost van Veen <joost@accentinteractive.nl>
2024-08-07 15:15:26 +0200

    Set default values for and added comments to config file.

--- config/sso.php
+++ config/sso.php
@@ -1,10 +1,43 @@
 <?php
 
 return [
-    'enabled' => env('SSO_ENABLED'),
+    /*
+     * Enable or disable Single Sign On.
+     */
+    'enabled' => env('SSO_ENABLED', false),
+
+    /*
+     * Register your application at https://portal.azure.com
+     * Copy the 'Application (client) ID'.
+     * For a full manual see README.
+     */
     'client_id' => env('SSO_CLIENT_ID'),
+
+    /*
+     * Register your application at https://portal.azure.com
+     * Copy the 'Directory (tenant) ID'.
+     * For a full manual see README.
+     */
     'tenant_id' => env('SSO_TENANT_ID'),
+
+    /*
+     * Register your application at https://portal.azure.com
+     * Register a new Client secret for the application.
+     * Copy the 'Value' of the new Client secret.
+     * For a full manual see README.
+     */
     'client_secret' => env('SSO_CLIENT_SECRET'),
+
+    /*
+     * Register your application at https://portal.azure.com
+     * Register a new Redirect URI for the application (Web).
+     * Copy the redirect URL for the application.
+     * For a full manual see README.
+     */
     'client_redirect_url' => env('SSO_CLIENT_REDIRECT_URL'),
-    'auth_tenant' => env('SSO_AUTH_TENANT'),
+
+    /*
+     * Is always 'common'.
+     */
+    'auth_tenant' => env('SSO_AUTH_TENANT', 'common'),
 ];
